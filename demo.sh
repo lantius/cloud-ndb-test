3222209c5e5c266ed85fa24abce067871b0210ae
Jon Grover <jon.grover@gmail.com>
2020-05-25 23:48:51 -0700

    Fixes to get app talking to local datastore emulator

--- demo.sh
+++ demo.sh
@@ -1,20 +1,15 @@
 #!/usr/bin/env bash
 
 source Venv/bin/activate
+export DATASTORE_DATASET=cloud-ndb-test
+export DATASTORE_EMULATOR_HOST_PATH=localhost:8089/datastore
+export DATASTORE_EMULATOR_HOST=localhost:8089
+export DATASTORE_HOST=http://localhost:8089
+export DATASTORE_PROJECT_ID=cloud-ndb-test
+
 RESULT=`curl -s http://localhost:8089`
 if [ "$RESULT" = "Ok" ]; then
-  python test.py \
-    --support_datastore_emulator=true \
-    --enable_host_checking=false \
-    --application=cloud-ndb-test \
-    --env_var DATASTORE_DATASET=cloud-ndb-test \
-    --env_var DATASTORE_EMULATOR_HOST_PATH=localhost:8089/datastore \
-    --env_var DATASTORE_HOST=http://localhost:8089 \
-    --env_var DATASTORE_PROJECT_ID=cloud-ndb-test \
-    --project=cloud-ndb-test \
-    app.yaml
+  python test.py
 else
   echo "Datastore emulator not detected on localhost:8089. Try running 'gcloud beta emulators datastore start --no-store-on-disk --consistency=1.0 --host-port=localhost:8089' in another terminal first."
 fi
-# dev_appserver.py app.yaml --enable_host_checking=false
-# python test.py
