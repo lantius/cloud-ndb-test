6fbdc2261ee735a8d9f3c64a7df73940ce541759
Lee Williams <lant@lantius.org>
2020-05-26 01:50:30 -0700

    Create an appenginendb approach that tries to do the same thing...

---
+++ appenginendb.py
@@ -0,0 +1,92 @@
+import subprocess
+import sys
+from requests import post
+VERSION = 'appengine-ndb'
+
+def init_appengine():
+  # default appengine path.
+  appengine_path = subprocess.check_output([
+      'bash', '-c',
+      'echo $(gcloud info --format="value(installation.sdk_root)")/platform/google_appengine'
+  ])
+  appengine_path = appengine_path.strip()
+  sys.path.insert(0, appengine_path)
+  try:
+    import dev_appserver
+  except ImportError:
+    print("ImportError importing dev_appserver. Check appengine_path.")
+    sys.exit(1)
+  dev_appserver.fix_sys_path()
+init_appengine()
+
+from google.appengine.ext import ndb
+context = ndb.get_context()
+context.set_cache_policy(False)
+context.set_memcache_policy(False)
+
+
+class ThreadMessage(ndb.Model):
+  """Models the individual messages in a thread."""
+  number = ndb.IntegerProperty(indexed=True)
+  created_by = ndb.StringProperty(default=VERSION)
+  text = ndb.TextProperty()
+  created = ndb.DateTimeProperty(auto_now=True, indexed=True)
+
+
+class Thread(ndb.Model):
+  """Models a thread of messages."""
+  created_by = ndb.StringProperty()
+  messages = ndb.LocalStructuredProperty(ThreadMessage,
+                                         repeated=True,
+                                         compressed=True)
+
+
+def get_or_create_thread():
+  thread = Thread.query().get()
+  if not thread:
+    thread = Thread(created_by=VERSION)
+    thread.put()
+  return thread
+
+
+def read_thread():
+  thread = get_or_create_thread()
+  if thread.messages:
+    for message in thread.messages:
+      print("#%s: Created in: %s || Text: %s" %
+            (message.number, message.created_by, message.text))
+  else:
+    print("No thread messages found.")
+
+
+def write_to_thread(text):
+  thread = get_or_create_thread()
+  number = len(thread.messages) + 1
+  message = ThreadMessage(number=number, text=text)
+  thread.messages.append(message)
+  thread.put()
+
+
+def reset_datastore():
+  post("http://localhost:8089/reset")
+  print("Resetting datastore!")
+
+
+if __name__ == '__main__':
+  response = 0
+  while response != 4:
+    print("Choose action:")
+    print("1. Read current messages")
+    print("2. Write a new message")
+    print("3. Reset datastore")
+    print("4. Quit")
+    response = input("Action to take: ")
+
+    if response == 1:
+      read_thread()
+    elif response == 2:
+      text = raw_input("What message to write?: ")
+      write_to_thread(text)
+    elif response == 3:
+      reset_datastore()
+    print("")
--- demo.sh
+++ demo.sh
@@ -1,15 +1,28 @@
 #!/usr/bin/env bash
 
+# gcloud beta emulators datastore start --project=cloud-ndb-test \
+#                                       --no-store-on-disk \
+#                                       --consistency=1.0 \
+#                                       --host-port=localhost:8089 \
+#                                       < /dev/null >& /dev/null &
+# dev_appserver.py \
+# --support_datastore_emulator=true \
+# --enable_host_checking=false \
+# --application=cloud-ndb-test \
+# app.yaml
+
 source Venv/bin/activate
 export DATASTORE_DATASET=cloud-ndb-test
 export DATASTORE_EMULATOR_HOST_PATH=localhost:8089/datastore
 export DATASTORE_EMULATOR_HOST=localhost:8089
 export DATASTORE_HOST=http://localhost:8089
 export DATASTORE_PROJECT_ID=cloud-ndb-test
+export DATASTORE_APP_ID=cloud-ndb-test
 
 RESULT=`curl -s http://localhost:8089`
 if [ "$RESULT" = "Ok" ]; then
-  python test.py
+  # python cloudndb.py
+  python appenginendb.py
 else
   echo "Datastore emulator not detected on localhost:8089. Try running 'gcloud beta emulators datastore start --no-store-on-disk --consistency=1.0 --host-port=localhost:8089' in another terminal first."
 fi
